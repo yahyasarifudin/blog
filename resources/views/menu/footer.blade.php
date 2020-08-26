b4cb847a984e22372c8dcebe47162eaf8ca2ea9f
Yahya Sarifudin <yahyasarifudin@gmail.com>
2020-08-26 10:28:37 +0700

    pecah header footer

---
+++ resources/views/menu/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.4
+    </div>
+  </footer>
+   <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+
+<script src="{{url('plugins/chart.js/Chart.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{url('plugins/sparklines/sparkline.js')}}"></script>
+
+<script src="{{url('plugins/moment/moment.min.js')}}"></script>
+
+<script src="{{url('plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{url('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{url('dist/js/demo.js')}}"></script>
+
+</body>
+</html>
